0307c9f551165ebe88cbbeb6db3a629bfc815201
Arnoldo226 <www.yaogojeux1234@gmail.com>
2024-07-31 23:46:42 +0100

    Add files via upload

--- scriptPatrimoine.js
+++ scriptPatrimoine.js
@@ -1,26 +1,17 @@
-let currentIndex = 0;
 
-function showSlide(index) {
-    const slides = document.querySelectorAll('.slide');
-    const totalSlides = slides.length;
-    if (index >= totalSlides) {
-        currentIndex = 0;
-    } else if (index < 0) {
-        currentIndex = totalSlides - 1;
-    } else {
-        currentIndex = index;
+var swiper = new Swiper(".mySwiper",{
+    effect: "coverflow",
+    grabCursor:true,
+    centeredSlider:true,
+    loop:true,
+    coverflowEffect: {
+        rotate:60,
+        stretch:0,
+        depth:100,
+        slideshadows:true,
+    },
+    slidesPerView:"auto",
+    pagination:{
+        el:".swiper-pagination"
     }
-    const slider = document.querySelector('.slider');
-    slider.style.transform = `translateX(-${currentIndex * 100}%)`;
-}
-
-function nextSlide() {
-    showSlide(currentIndex + 1);
-}
-
-function prevSlide() {
-    showSlide(currentIndex - 1);
-}
-
-// Initialize the first slide
-showSlide(currentIndex);
+})
